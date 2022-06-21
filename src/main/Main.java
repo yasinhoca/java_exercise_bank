5614993d9c70f30d362d573dccfe834cee06aa0a
Yasin Hoca <myasinozsaglam@gmail.com>
2022-06-21 15:16:29 +0300

    Update Main.java

--- src/main/Main.java
+++ src/main/Main.java
@@ -3,7 +3,7 @@
 //I understand that the only sources of authorized information in this lab assignment are (1) the course textbook, 
 //(2) the materials posted at the course website and (3) any study notes handwritten by myself. 
 //I have not used, accessed or received any information from any other unauthorized source in taking this lab assignment. 
-//The effort in the assignment thus belongs completely to me. READ AND SIGN BY WRITING YOUR NAME SURNAME AND STUDENT ID SIGNATURE:Mehmet Furkan Geckil 68944
+//The effort in the assignment thus belongs completely to me. READ AND SIGN BY WRITING YOUR NAME SURNAME AND STUDENT ID SIGNATURE:
 
 package main;
 
